e6adac25bc1e614d19bfa25b6c979673e89e8b7a
pogromister <tarasmalarcuk8@gmail.com>
2025-06-10 20:46:49 +0300

    feat: add header in app layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
-import Image from "next/image";
 import ProtectedRoute from "@/shared/ui/ProtectedRoute";
-import LogoutButton from "@/features/auth/ui/LogoutButton";
-import UserInfo from "@/features/user/ui/UserInfo";
 
 export default function Home() {
   return (
       <ProtectedRoute>
           <div
               className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-              <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                  <Image
-                      className="dark:invert"
-                      src="/next.svg"
-                      alt="Next.js logo"
-                      width={180}
-                      height={38}
-                      priority
-                  />
-                  <UserInfo/>
-                  <LogoutButton/>
-              </main>
+              there will be content
           </div>
       </ProtectedRoute>
 
--- src/features/user/ui/UserInfo.tsx
+++ src/features/user/ui/UserInfo.tsx
@@ -3,20 +3,21 @@
 import { useAuth } from "@/features/auth/model/useAuth";
 import { useUserProfile } from "../model/useUserProfile";
 
+
 export default function UserInfo() {
     const { user } = useAuth();
     const { profile } = useUserProfile(user?.uid);
 
-    if (!user) return null;
-    if (!profile) return <div>Loading...</div>;
+    if (!user || !profile) return null;
 
     return (
-        <div className="flex items-center gap-4 p-4 bg-gray-100 rounded">
-            <img src={profile.photoURL} alt="avatar" className="w-10 h-10 rounded-full" />
-            <div>
-                <div className="font-bold">{profile.displayName}</div>
-                <div className="text-sm text-gray-500">{profile.email}</div>
-            </div>
+        <div className="flex items-center gap-2">
+            <img
+                src={profile.photoURL || `https://ui-avatars.com/api/?name=${encodeURIComponent(profile.displayName || "")}`}
+                alt="avatar"
+                className="w-8 h-8 rounded-full border"
+            />
+            <span className="font-medium">{profile.displayName}</span>
         </div>
     );
 }
